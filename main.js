2d390b5608c3cddb65816f4eee1ae008bd8fac10
naaru2011 <83050539+naaru2011@users.noreply.github.com>
2022-06-02 16:21:10 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+song = "";
+song2 = "";
+function preload(){
+    song = loadSound("NeKo Music - Za Warudo.mp3");
+    song2 = loadSound("Dm Dokuro - Scourge of the Universe.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
